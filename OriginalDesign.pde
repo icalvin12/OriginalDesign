37bd17e130753210cc96f17edeceba311f8aad3c
icalvin12 <calvin941127@aol.com>
2014-08-26 11:34:27 -0700

    index.html

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,12 +1,66 @@
+int x = 250;
+int y = 100;
+int changeX = 10;
+int changeY = 7;
+int width = 50;
+int height = 50;
+boolean pause = false;
+
 void setup()
 {
-  size(300,300);
+  size(500,500);
 }
 
 void draw()
 {
-  fill(255,0,0);
-  rectMode(CENTER);
-  rect(150,150,5,5);
+	if(pause == true)
+	{
+		pause();
+	}
+	else 
+	{
+		ball();		
+	}
 }
 
+void ball()
+{
+	fill(0,0,0,50);
+	rect(0,0,500,500);
+	noFill();
+	stroke(255,255,255);
+	ellipse(x, y, width, height);
+	if(x>475)
+	{
+		changeX = changeX *-1;
+	}
+	if(x<25)
+	{
+		changeX = changeX *-1;
+	}
+	if(y>475)
+	{
+		changeY = changeY *-1;
+	}
+	if(y<25)
+	{
+		changeY = changeY *-1;
+	}
+	x = x + changeX;
+	y = y + changeY;
+}
+
+void pause()
+{
+
+	rect(200, 200, 40, 100);
+	rect(260, 200, 40, 100);
+}
+
+void keyPressed()
+{
+	if(key == 'p')
+	{
+		pause = !pause;
+	}
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,72 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+int x = 250;
+int y = 100;
+int changeX = 10;
+int changeY = 7;
+int width = 50;
+int height = 50;
+boolean pause = false;
+
 public void setup()
 {
-
+  size(500,500);
 }
+
 public void draw()
 {
-  ellipse(50,50,50,50);
+	if(pause == true)
+	{
+		pause();
+	}
+	else 
+	{
+		ball();		
+	}
 }
 
+public void ball()
+{
+	fill(0,0,0,50);
+	rect(0,0,500,500);
+	noFill();
+	stroke(255,255,255);
+	ellipse(x, y, width, height);
+	if(x>475)
+	{
+		changeX = changeX *-1;
+	}
+	if(x<25)
+	{
+		changeX = changeX *-1;
+	}
+	if(y>475)
+	{
+		changeY = changeY *-1;
+	}
+	if(y<25)
+	{
+		changeY = changeY *-1;
+	}
+	x = x + changeX;
+	y = y + changeY;
+}
+
+public void pause()
+{
+
+	rect(200, 200, 40, 100);
+	rect(260, 200, 40, 100);
+}
+
+public void keyPressed()
+{
+	if(key == 'p')
+	{
+		pause = !pause;
+	}
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
