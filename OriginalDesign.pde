d20a67e7005da657da9b9cbd559f2bbab3143247
icalvin12 <calvin941127@aol.com>
2014-08-28 11:17:06 -0700

    index.html

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -4,11 +4,14 @@ int changeX = 10;
 int changeY = 7;
 int width = 50;
 int height = 50;
+int r,g,b;
 boolean pause = false;
+boolean circle = true;
 
 void setup()
 {
   size(500,500);
+  rectMode(CENTER);
 }
 
 void draw()
@@ -19,32 +22,47 @@ void draw()
 	}
 	else 
 	{
-		ball();		
+		ball();
+		mouseWheel();		
 	}
 }
 
 void ball()
 {
+	r = int(random(0, 255));
+	g = int(random(0, 255));
+	b = int(random(0, 255));
 	fill(0,0,0,50);
-	rect(0,0,500,500);
+	rect(250,250,550,550);
 	noFill();
-	stroke(255,255,255);
-	ellipse(x, y, width, height);
-	if(x>475)
+	stroke(r,g,b);
+	if(circle==true)
+	{
+		ellipse(x, y, width, height);
+	}
+	else 
+	{
+		rect(x,y,width,height);
+	}
+	if(x>500-(width/2))
 	{
 		changeX = changeX *-1;
+		x = x + changeX;
 	}
-	if(x<25)
+	if(x<(width/2))
 	{
 		changeX = changeX *-1;
+		x = x + changeX;
 	}
-	if(y>475)
+	if(y>500-(width/2))
 	{
 		changeY = changeY *-1;
+		y = y + changeY;
 	}
-	if(y<25)
+	if(y<(width/2))
 	{
 		changeY = changeY *-1;
+		y = y + changeY;
 	}
 	x = x + changeX;
 	y = y + changeY;
@@ -63,4 +81,25 @@ void keyPressed()
 	{
 		pause = !pause;
 	}
+}
+
+void mousePressed()
+{
+	circle = !circle;
+}
+
+void mouseWheel(MouseEvent event) 
+{
+  width = width + (2*event.getCount());
+  height = height + (2*event.getCount());
+  if(width > 200)
+  {
+  	width = 200;
+  	height = 200;
+  }
+  if(width < 5)
+  {
+  	width = 5;
+  	height = 5;
+  }
 }
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -20,11 +20,14 @@ int changeX = 10;
 int changeY = 7;
 int width = 50;
 int height = 50;
+int r,g,b;
 boolean pause = false;
+boolean circle = true;
 
 public void setup()
 {
   size(500,500);
+  rectMode(CENTER);
 }
 
 public void draw()
@@ -35,32 +38,47 @@ public void draw()
 	}
 	else 
 	{
-		ball();		
+		ball();
+		mouseWheel();		
 	}
 }
 
 public void ball()
 {
+	r = PApplet.parseInt(random(0, 255));
+	g = PApplet.parseInt(random(0, 255));
+	b = PApplet.parseInt(random(0, 255));
 	fill(0,0,0,50);
-	rect(0,0,500,500);
+	rect(250,250,550,550);
 	noFill();
-	stroke(255,255,255);
-	ellipse(x, y, width, height);
-	if(x>475)
+	stroke(r,g,b);
+	if(circle==true)
+	{
+		ellipse(x, y, width, height);
+	}
+	else 
+	{
+		rect(x,y,width,height);
+	}
+	if(x>500-(width/2))
 	{
 		changeX = changeX *-1;
+		x = x + changeX;
 	}
-	if(x<25)
+	if(x<(width/2))
 	{
 		changeX = changeX *-1;
+		x = x + changeX;
 	}
-	if(y>475)
+	if(y>500-(width/2))
 	{
 		changeY = changeY *-1;
+		y = y + changeY;
 	}
-	if(y<25)
+	if(y<(width/2))
 	{
 		changeY = changeY *-1;
+		y = y + changeY;
 	}
 	x = x + changeX;
 	y = y + changeY;
@@ -79,6 +97,27 @@ public void keyPressed()
 	{
 		pause = !pause;
 	}
+}
+
+public void mousePressed()
+{
+	circle = !circle;
+}
+
+public void mouseWheel(MouseEvent event) 
+{
+  width = width + (2*event.getCount());
+  height = height + (2*event.getCount());
+  if(width > 200)
+  {
+  	width = 200;
+  	height = 200;
+  }
+  if(width < 5)
+  {
+  	width = 5;
+  	height = 5;
+  }
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
