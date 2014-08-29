ab6687725797dead4c32c8fd8cc0cad2b0133ebb
icalvin12 <calvin941127@aol.com>
2014-08-29 11:52:15 -0700

    originaldesign.pde

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
 int x = 250;
 int y = 100;
 int changeX = 10;
@@ -8,13 +24,13 @@ int r,g,b;
 boolean pause = false;
 boolean circle = true;
 
-void setup()
+public void setup()
 {
 	rectMode(CENTER);
 	size(500,500);
 }
 
-void draw()
+public void draw()
 
 {	if(pause == true)
 	{
@@ -36,11 +52,11 @@ void draw()
  	}
 }
 
-void ball()
+public void ball()
 {
-	r = int(random(0, 255));
-	g = int(random(0, 255));
-	b = int(random(0, 255));
+	r = PApplet.parseInt(random(0, 255));
+	g = PApplet.parseInt(random(0, 255));
+	b = PApplet.parseInt(random(0, 255));
 	fill(0,0,0,50);
 	rect(250,250,550,550);
 	noFill();
@@ -77,13 +93,13 @@ void ball()
 	y = y + changeY;
 }
 
-void paused()
+public void paused()
 {
 	rect(280, 250, 40, 100);
 	rect(220, 250, 40, 100);
 }
 
-void keyPressed()
+public void keyPressed()
 {
 	if(key == 'p')
 	{
@@ -104,7 +120,16 @@ void keyPressed()
 	}
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	circle = !circle;
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
